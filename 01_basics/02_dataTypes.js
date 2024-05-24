adbb78e22ce6f1ed6039137e3a1d8a9a2f5aa6a6
sotrider <107476111+sotrider@users.noreply.github.com>
2024-05-24 21:47:42 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser 
+
+console.log(3 + 3) 
+console.log("Sushant")
+
+let name = "Sushant"
+let age = 23
+let isLoggedIn = false
+let state;
+
+// number => to power 53
+// bigint 
+// string => ""
+// boolean => true/flase
+// null => standalone value 
+// undefined => 
+// symbol => unique
+
+// object 
+
+console.log(typeof "Sushant");
+console.log(typeof age);
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined 
