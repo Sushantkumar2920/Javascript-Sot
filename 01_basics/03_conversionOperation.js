52731e00d6bbfeaf967bb847e66b03bf5765f6f4
sotrider <107476111+sotrider@users.noreply.github.com>
2024-10-12 10:42:57 +0000

    Operations in datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = "33abc"
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 //"33ab" => NaN
@@ -13,7 +13,7 @@ console.log(valueInNumber);
 
 let iSLoggedIn = 1
 let booleanIsLoggedIn = Boolean(iSLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 = false
 // "" => false
@@ -21,5 +21,41 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+//********************************Operations********************************
+
+let value = 3
+let negValue = -value
+// console.log( negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2%2);
+
+let str1 = "Hello"
+let str2 = " Sush"
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log(1+"2");
+// console.log("1"+2);
+// console.log("1"+2+2);
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2 
+
+let gameCounter =100
+gameCounter++;
+console.log(gameCounter);
+
