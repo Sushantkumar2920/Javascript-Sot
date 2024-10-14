d65b0d7657134ac8c4b3c2e0be916f3e2c927409
Sushant Kumar <107476111+Sushantkumar2920@users.noreply.github.com>
2024-10-14 22:37:56 +0000

    objects

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -16,15 +16,15 @@ const JsUser = {
     lastLoginDays: ["monday", "saturday"]
 }
 
-// console.log(JsUser.email);         // in object tihs is the type to fetch data  
-// console.log(JsUser["email"]);      // in object tihs is the type to fetch data
-// console.log(JsUser["full name"])   // like this we can sprint the keywords
-// console.log(JsUser[mySym]);        // by this way we can print symbols
+console.log(JsUser.email);         // in object tihs is the type to fetch data  
+console.log(JsUser["email"]);      // in object tihs is the type to fetch data
+console.log(JsUser["full name"])   // like this we can sprint the keywords
+console.log(JsUser[mySym]);        // by this way we can print symbols
 
 JsUser.email = "chotu@gmail.com"   // we can give new value & we can freeze it
 // Object.freeze(JsUser)              // we can freeze the value of selected value so that we cannot change in the future
 JsUser.email = "sush@apple.com"
-// console.log(JsUser);
+console.log(JsUser);
 
 
 JsUser.greeting = function() {
