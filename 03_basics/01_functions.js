73b2de32216e07da813f4cdb7ed813f7736b3a3c
Sushant Kumar <107476111+Sushantkumar2920@users.noreply.github.com>
2024-10-15 23:21:28 +0000

    Functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,18 @@
+
+function sayMyName() {
+    console.log("S");
+    console.log("U");
+    console.log("S");
+    console.log("H");
+    console.log("A");
+    console.log("N");
+    console.log("T");
+}
+
+// sayMyName() 
+
+function addTwoNumbers(number1, number2){
+    console.log(number1 + number2);
+}
+
+addTwoNumbers(3, 5);
