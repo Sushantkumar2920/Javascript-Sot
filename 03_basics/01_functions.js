d5d6a0de6446cb565f93c3b6c67300eac739b4ef
Sushant Kumar <107476111+Sushantkumar2920@users.noreply.github.com>
2024-10-16 11:26:47 +0000

    Functions in depth

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -11,8 +11,30 @@ function sayMyName() {
 
 // sayMyName() 
 
-function addTwoNumbers(number1, number2){
-    console.log(number1 + number2);
+// function addTwoNumbers(number1, number2){        //this is function and inside this() parameter 
+//     console.log(number1 + number2);
+// }
+
+function addTwoNumbers(number1, number2){        //this is function and inside this() parameter 
+    // let result = number1 + number2
+    // return result   
+    return number1 + number2                        // this is also the way to get the return value 
+    // console.log("Sushant");                      // when will print something after return it wil never execute that code 
 }
 
-addTwoNumbers(3, 5);
+
+// addTwoNumbers(3, 5);                       // when we call a function its an argument it can be anything number, string, etc..
+const result = addTwoNumbers(3, 5)         // we can kept this function call under variable 
+
+// console.log("Result:", result);
+
+function loginUserMessage(username = "Sush"){      // in a function we can assign default value also in the parameter
+    if(!username){
+        console.log("Please enter username");
+        return
+    }
+    return `${username} just logged in `
+}
+
+// console.log(loginUserMessage("Sushant"));
+console.log(loginUserMessage("Sushant"));       // in this we can overwrite the default value 
