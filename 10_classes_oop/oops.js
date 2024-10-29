6e390d904e02dbe1a005f2dc6145446778c0e1c5
Sushantkumar2920 <sushantraj2920@gmail.com>
2024-10-29 17:54:35 +0530

    Class

--- 10_classes_oop/oops.js
+++ 10_classes_oop/oops.js
@@ -13,9 +13,27 @@ const user = {
 
 
 
-console.log(user.username);
-console.log(user.getUserDetails());
-console.log(this);
+// console.log(user.username);
+// console.log(user.getUserDetails());
+// console.log(this);
 
-const promiseOne = new Promise()
-const date = new Date()
+// const promiseOne = new Promise()
+// const date = new Date()
+
+function User(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn
+
+    this.greeting = function(){
+        console.log(`welcome ${this.username}`);
+        
+    }
+
+    return this
+}
+
+const userOne = new User("sushant", 12, true)
+const userTwo = new User("sush", 11, false)
+console.log(userOne.constructor);
+// console.log(userTwo);
