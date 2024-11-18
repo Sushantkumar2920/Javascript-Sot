7e197413b50c60ca1d1738641c4a8c09d525e8bc
Sushantkumar2920 <sushantraj2920@gmail.com>
2024-11-18 15:19:29 +0530

    classes

--- 10_classes_oop/myclasses.js
+++ 10_classes_oop/myclasses.js
@@ -1,16 +1,40 @@
-//ES6 
+// // ES6
 
-class User{
-    constructor(username, email, password){
-        this.username;
-        this.email;
-        this.password;
-    }
+// class User {
+//     constructor(username, email, password){
+//         this.username = username;
+//         this.email = email;
+//         this.password = password
+//     }
 
-    encryptPassword(){
-        return `${this.password}abc`
-    }
+//     encryptPassword(){
+//         return `${this.password}abc`
+//     }
+//     changeUsername(){
+//         return `${this.username.toUpperCase()}`
+//     }
+// }
+
+// const sush = new User("sush", "chai@gmail.com", "123")
+// console.log(sush.encryptPassword());
+// console.log(sush.changeUsername());
+
+// behind the scene
+
+function User(username, email, password){
+    this.username = username;
+    this.email = email;
+    this.password = password
 }
 
-const sush = new User("sush", "sush@gmail.com", "123")
-console.log(sush.encryptPassword());
+User.prototype.encryptPassword = function(){
+    return `${this.password}abc` 
+}
+
+User.prototype.changeUsername = function(){
+    return `${this.username.toUpperCase()}` 
+}
+
+const susha = new User("susha", "susha@gmail.com", "123")
+console.log(susha.encryptPassword());
+console.log(susha.changeUsername());
